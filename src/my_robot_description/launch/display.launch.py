4821c56ad56c299e24bd112c30bc44b2e354b945
Dmytro Yerusalimtsev <dima.yerusalimtsev@gmail.com>
2023-11-21 11:33:25 +0200

    Added launch files for gviz2 and my model.

---
+++ src/my_robot_description/launch/display.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.actions import Node
+from launch.substitutions import Command
+from ament_index_python.packages import get_package_share_path
+import os
+
+def generate_launch_description():
+    urdf_path = os.path.join(get_package_share_path('my_robot_description'), 'urdf', 'my_robot.urdf')
+    rviz_config_path = os.path.join(get_package_share_path('my_robot_description'), 'rviz', 'config.rviz')
+
+    robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[
+            {'robot_description': robot_description}
+        ]
+    )
+
+    joint_state_publisher = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui'
+    )
+
+    rviz2 = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', rviz_config_path]
+    )
+
+    return LaunchDescription([
+        robot_state_publisher,
+        joint_state_publisher,
+        rviz2
+    ])
